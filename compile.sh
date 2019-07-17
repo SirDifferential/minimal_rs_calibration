0e59bba1660886311505dfa28098792c5e76a6ef
Jesse Kaukonen <jesse.kaukonen@gmail.com>
2019-07-17 12:16:45 +0300

    have to compile the code as cpp

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ main.c -I/home/gekko/librealsense/include -I/home/gekko/librscalibrationapi/include -L/home/gekko/librealsense/build -lrealsense2 -L/home/gekko/librscalibrationapi/lib -lDSDynamicCalibrationAPI
+g++ main.cpp -I/home/gekko/librealsense/include -I/home/gekko/librscalibrationapi/include -L/home/gekko/librealsense/build -lrealsense2 -L/home/gekko/librscalibrationapi/lib -lDSDynamicCalibrationAPI
